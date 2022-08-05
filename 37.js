b7809d81f2b5fbceec882128fe66dd0a631aa63a
Zhanett Serikbayeva <109654778+zhanettprog@users.noreply.github.com>
2022-08-05 22:56:44 +0600

    Add files via upload

---
+++ 37.js
@@ -0,0 +1,9 @@
+const myNoun = "dog";
+const myAdjective = "big";
+const myVerb = "ran";
+const myAdverb = "quickly";
+
+// Only change code below this line
+const wordBlanks =
+  "My " + myNoun + " " + myAdjective + " named Koki " + myVerb + " " + myAdverb; // Change this line
+// Only change code above this line
